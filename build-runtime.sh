a0efb34b070090644525dc896473a458ad70e7a1
Wei Huang <Wei.Huang@noaa.gov>
2025-09-19 21:42:42 +0000

    add initial file

--- build-runtime.sh
+++ build-runtime.sh
@@ -8,4 +8,5 @@
 
  sudo singularity build intel-runtime-2024.2.sif intel-runtime.def
  sudo singularity build intel-hpc-2024.2-mpi-2021.13.sif intel-hpc.def
+ sudo singularity build gw-runtime.sif gw-runtime.def
 
