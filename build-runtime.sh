60f6f834eba787d79061698ce90dda28f2665c41
Wei Huang <Wei.Huang@noaa.gov>
2025-09-24 20:48:45 +0000

    build runtime without GW

--- build-runtime.sh
+++ build-runtime.sh
@@ -6,7 +6,22 @@
  export SINGULARITY_CACHEDIR=/lustre/$USER/cache
  export SINGULARITY_TMPDIR=/lustre/$USER/tmp
 
- sudo singularity build intel-runtime-2024.2.sif intel-runtime.def
- sudo singularity build intel-hpc-2024.2-mpi-2021.13.sif intel-hpc.def
- sudo singularity build gw-runtime.sif gw-runtime.def
+ export MY_LUSTRE_TMP=/lustre/$USER/singularity-builds
+ mkdir -p $MY_LUSTRE_TMP
 
+ export SINGULARITY_TMPDIR=$MY_LUSTRE_TMP
+ export APPTAINER_TMPDIR=$MY_LUSTRE_TMP
+
+
+
+#sudo singularity build intel-runtime-2024.2.sif intel-runtime.def
+#sudo singularity build intel-hpc-2024.2-mpi-2021.13.sif intel-hpc.def
+#sudo singularity build gw-runtime.sif gw-runtime.def
+
+#Build a sandbox
+#sudo singularity build --sandbox gw-runtime gw-runtime.sif
+
+#convert sandbox to SIF
+#sudo singularity build new_gw-runtime.sif gw-runtime
+
+ sudo singularity build intel-hpc-2024.2-mpi-2021.13-ss192.sif runtime-ss192.def
